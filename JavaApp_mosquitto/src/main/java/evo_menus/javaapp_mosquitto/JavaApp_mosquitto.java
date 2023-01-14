0169a1fe019be2d27fef3d30898e98ac7982d798
JoelCMateus <2211908@my.ipleiria.pt>
2023-01-14 17:04:49 +0000

    Aplicação em java para server de cliente MQTT

---
+++ JavaApp_mosquitto/src/main/java/evo_menus/javaapp_mosquitto/JavaApp_mosquitto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package evo_menus.javaapp_mosquitto;
+
+/**
+ *
+ * @author joelm
+ */
+public class JavaApp_mosquitto {
+
+    public static void main(String[] args) {
+        ConnectionForm connectionForm = new ConnectionForm();
+        connectionForm.setVisible(true);
+    }
+}
